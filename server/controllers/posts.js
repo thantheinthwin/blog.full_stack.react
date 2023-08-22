a115b79b80ceeca4dd7cf78eb49893302c512403
thantheinthwin <thantheinthwin.dev@gmail.com>
2023-08-22 11:56:52 +0630

    api for getting all posts added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,13 @@
+import { db } from "../db.js";
+
 export const getPosts = (req, res) => {
+    const q = "SELECT * FROM posts";
+
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        
+        return res.status(200).json(data);
+    })
     res.json("get posts")
 }
 
